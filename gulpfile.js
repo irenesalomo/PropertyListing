a358bf797bb87771843573eaf286994a19672ab0
irenesalomo <irene.salomo@gmail.com>
2018-02-16 09:09:35 +1100

    update README, development notes, cleanup gulpfile, commit package-lock.json

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var rename = require('gulp-rename');
 var less = require('gulp-less');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify-es').default;
 var replace = require('gulp-replace');
 
 gulp.task('less', function () {
@@ -14,15 +13,7 @@ gulp.task('less', function () {
       .pipe(gulp.dest('src/css/build/'));
 });
 
-gulp.task('js', function () {
-  return gulp.src(['src/app.js',])
-      .pipe(uglify())
-      .pipe(rename({extname: '.min.js'}))
-      .pipe(gulp.dest('src/'));
-});
-
 gulp.task('watch', function () {  // Watch .scss files
   gulp.watch('src/css/less/*.less', ['less']);  // Watch .less files
-  gulp.watch('src/*.js', ['js']);  // Watch .less files
 });
 
