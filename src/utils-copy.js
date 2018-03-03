f8a7931b1a4f85f861ab962d5231c546cd75825c
Irene Salomo <irene.salomo@gmail.com>
2018-03-03 16:10:44 +1100

    refactor utils.js to use object instead of function as factory

---
+++ src/utils-copy.js
@@ -0,0 +1,30 @@
+/* --- Internal Helper Methods --- */
+// This util uses function as factory
+define(function() {
+    return {
+        //Show confirmation text after user clicks save button
+        showConfirmation: function(targetSibling, confirmationClass, confirmationText) {
+            $(targetSibling).siblings(confirmationClass)
+                            .text(confirmationText)
+                            .fadeIn( "fast", function() {
+                                $(confirmationClass).delay(2000).fadeOut("slow"); 
+                            });
+        }, 
+        // Remove property ID from savedPropertyID array
+        removeSavedProperty : function (savedProperty, propertyID) {
+            const index = savedProperty.indexOf(propertyID);
+            savedProperty.splice(index, 1);
+        },
+        // Add property ID into savedPropertyID array
+        addSavedProperty : function (savedProperty, propertyID) {
+            // Check if this property has already exists on savedPropertyID array, if not then save the ID 
+            if (savedProperty.indexOf(propertyID) === -1) {
+                savedProperty.push(propertyID);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+});
--- src/utils.js
+++ src/utils.js
@@ -1,32 +1,27 @@
 /* --- Internal Helper Methods --- */
-define(function(utils) {
-    return {
-        //Show confirmation text after user clicks save button
-        showConfirmation: function(targetSibling, confirmationClass, confirmationText) {
-            $(targetSibling).siblings(confirmationClass)
-                            .text(confirmationText)
-                            .fadeIn( "fast", function() {
-                                $(confirmationClass).delay(2000).fadeOut("slow"); 
-                            });
-        }, 
-        // Remove property ID from savedPropertyID array
-        removeSavedProperty : function (savedProperty, propertyID) {
-            const index = savedProperty.indexOf(propertyID);
-            savedProperty.splice(index, 1);
-        },
-        // Add property ID into savedPropertyID array
-        addSavedProperty : function (savedProperty, propertyID) {
-            // Check if this property has already exists on savedPropertyID array, if not then save the ID 
-            if (savedProperty.indexOf(propertyID) === -1) {
-                savedProperty.push(propertyID);
-                return true;
-            }
-            else {
-                return false;
-            }
-        },
-        // eventListener: function(el, evt, context, callback){
-        //     el.on(evt, context, fn);
-        // },
+// This util uses object as factory
+define({
+    showConfirmation: function(targetSibling, confirmationClass, confirmationText) {
+        $(targetSibling).siblings(confirmationClass)
+                        .text(confirmationText)
+                        .fadeIn( "fast", function() {
+                            $(confirmationClass).delay(2000).fadeOut("slow"); 
+                        });
+    }, 
+    removeSavedProperty : function (savedProperty, propertyID) {
+        const index = savedProperty.indexOf(propertyID);
+        savedProperty.splice(index, 1);
+    },
+    addSavedProperty : function (savedProperty, propertyID) {
+        // Check if this property has already exists on savedPropertyID array, if not then save the ID 
+        if (savedProperty.indexOf(propertyID) === -1) {
+            savedProperty.push(propertyID);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
+
+
 });
