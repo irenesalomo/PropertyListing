6070269359069b6f775d408d4a25d25d7510bd83
Irene Salomo <irene.salomo@gmail.com>
2018-03-05 22:41:15 +1100

    WIP write filter function

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ define(["jquery", "utils", "handlebars"], function ($, utils, handlebars) {
         // To retrieve saved property information based on the property ID on savedProperty array, to be rendered on template
         // TODO: Need to handle edge case when result and saved properties has same element at initial state. Although this is currently not the case with the given JSON. 
         getSavedPropertyData : function() {
+            debugger;
             var savedPropertiesData = propertyLibrary.properties.filter(function(property) {
                 return savedPropertyID.indexOf(property.id) !== -1;
             });
@@ -55,6 +56,7 @@ define(["jquery", "utils", "handlebars"], function ($, utils, handlebars) {
             if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
                 // If request is done & successful, combine result & saved properties into 1 object, store the saved properties ID into 1 object & display them
                 var resultProperties = request.response.results;
+                console.log(resultProperties);
                 var savedProperties = request.response.saved;
                 savedProperties.forEach (function(property){
                     savedPropertyID.push(property.id);
@@ -66,6 +68,10 @@ define(["jquery", "utils", "handlebars"], function ($, utils, handlebars) {
                 handlebars.updateSavedPropertyButton();
                 apiMethods.bindEvents();
                 propertyLibrary.properties = resultProperties.concat(savedProperties);
+                // propertyLibrary.properties = resultProperties.concat(savedProperties.filter(function (property){
+                //     return resultProperties.id.indexOf(property === -1);
+                // })
+                // );
             }
             else
                 console.log("false");
